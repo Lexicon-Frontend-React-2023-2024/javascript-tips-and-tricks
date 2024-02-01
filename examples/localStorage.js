0238c83737f6dfbce246cc55996fc0a356ba991c
Niklas Fähnrich <niklasfahnrich@hotmail.com>
2024-02-01 10:32:53 +0100

    add localstorage section and an example regarding it

---
+++ examples/localStorage.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const todoList = document.querySelector(".todo-list");
+const TODOS = "todos";
+let todos;
+
+const rawTodos = localStorage.getItem(TODOS);
+
+if (!rawTodos) {
+  todos = [];
+  console.log("No saved todos");
+} else {
+  todos = JSON.parse(rawTodos);
+  const todosHTML = todos.map((todo) => createTodo(todo)).join("");
+  todoList.innerHTML = todosHTML;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newTodo = input.value;
+
+  todos.unshift(newTodo);
+  localStorage.setItem(TODOS, JSON.stringify(todos));
+
+  const newTodoHTML = createTodo(newTodo);
+  todoList.insertAdjacentHTML("afterbegin", newTodoHTML);
+  input.value = "";
+});
+
+function createTodo(todo) {
+  return /*html*/ `
+  <article>
+    <span>${todo}</span>
+  </article>
+  `;
+}
